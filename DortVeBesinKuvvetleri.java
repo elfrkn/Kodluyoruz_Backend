0f6da51c11f94def2b7442b2a01fde8883c3f875
Elif ÇALIŞ <101409313+elfrkn@users.noreply.github.com>
2022-10-10 20:50:08 +0300

    Update DortVeBesinKuvvetleri.java

--- DortVeBesinKuvvetleri.java
+++ DortVeBesinKuvvetleri.java
@@ -7,12 +7,12 @@ public class İkininKuvvetleri {
         System.out.println("Sınır sayısını giriniz:");
         n=input.nextInt();
         for(int i=1; i<=n; i*=4){
-            System.out.println("4'ün kuvvetleri:" +i);
+            System.out.print(i + " ");
         }
-        System.out.println("------------------");
+        System.out.println(" ");
         for(int j=1; j<=n; j*=5){
 
-                System.out.println("5'in kuvvetleri:" +j);
+                System.out.print(j+" ");
             }
 
     }
