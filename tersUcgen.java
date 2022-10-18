247602e93ff967bb919aae2fffa577e2269c242b
Elif ÇALIŞ <101409313+elfrkn@users.noreply.github.com>
2022-10-18 14:58:30 +0300

    Ters üçgen
    Girilen basamak sayısına göre ters üçgen çizimi.

---
+++ tersUcgen.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TersUcgen {
+    public static void main(String[] args) {
+
+        Scanner in=new Scanner(System.in);
+
+        System.out.print("Ters üçgen için N sayısını giriniz:");
+        int n=in.nextInt();
+
+        for(int i=(n-1); i>=1; i--){
+            for(int k=1; k<=(n-i); k++){
+                System.out.print(" ");
+            }
+
+            for(int j=1; j<=(2*i)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+}
